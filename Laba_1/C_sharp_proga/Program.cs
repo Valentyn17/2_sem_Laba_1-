7329843f46669ece14c511064a32f1cff96ba857
Valentyn17 <54958519+Valentyn17@users.noreply.github.com>
2020-02-18 20:56:47 +0200

    Add files via upload

---
+++ Laba_1/C_sharp_proga/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C_sharp_proga
+{
+    class Program
+    {
+        static void inkrement(ref int b)
+        {
+            int x = 0;
+            while ((b & (1 << x)) != 0)
+            {
+                b ^= (1 << x);
+                x++;
+            }
+            b ^=(1 << x);
+        }
+
+        static string chy_bilshe(int number1, int number2)
+        {
+
+            int key = Convert.ToString(Math.Abs(Math.Max(number1, number2)), 2).Length-1;
+            for (int i=key;i>=0;i--)
+            {
+                if ((number1 & (number1 << i)) > (number2 & (number2 << i)))
+                {
+                    return $"Число {number1} бiльше нiж {number2}";
+                }
+            }
+            return $"Число {number1} не бiльше ніж {number2}";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть  число, яке будемо збiльшувати:");
+            int a=Convert.ToInt32(Console.ReadLine());
+            inkrement(ref a);
+            Console.WriteLine("Iнкремент цього числа {0}", a);
+            Console.WriteLine("Введiть два числа, якi будемо перевiряти операцiєю вiдношення  '>'  :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine(chy_bilshe(num1,num2));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Laba_1/Laba_1/Laba_1.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+bool chy_bilshe(int, int);
+void inkrement(int&);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a;
+	cout << "Введiть число яке будемо iнкрементувати : ";
+	cin >> a;
+	cout << "Результат : ";
+    inkrement(a);
+	cout << a << endl;
+	
+	int n1, n2;
+	cout<<"Введiть два числа якi будемо порiвнювати знаком '>': "<<endl;
+	cin >> n1 >> n2;
+	if (chy_bilshe(n1, n2))
+		cout << "Число " << n1 << " бiльше за число " << n2 << endl;
+	else
+		cout<< "Число " << n1 << "не бiльше за число " << n2 << endl;
+	system("pause");
+}
+
+
+void inkrement(int &b)
+{
+	int x = 0;
+	while ((b & (1 << x)) != 0)
+	{
+		b = b ^ (1 << x);
+		x++;
+	}
+	b = b ^ (1 << x);
+
+}
+
+
+bool chy_bilshe(int number1, int number2)
+{
+	int k =to_string(abs(max(number1, number2))).length();
+	bool key=false;
+	for (int i = k; i >= 0; i--)
+	{
+		if ((number1 & (number1 << i)) > (number2 & (number2 << i)))
+		{
+			key = true;
+		}
+	}
+	return key;
+}
+
+
+//string v_dva(int a)
+//{
+//	string chyslo="";
+//	while (a > 0)
+//	{
+//		chyslo.insert(0, to_string(a % 2));
+//		a /= 2;
+//	}
+//	return chyslo;
+//}
